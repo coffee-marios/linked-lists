122cec815f569ea35af940e432fd505231646b38
coffee-marios <mark_theo@yahoo.com>
2024-06-08 11:09:09 +0300

    Create a function (size) that returns the total number of nodes in the list

--- index.js
+++ index.js
@@ -42,6 +42,17 @@ class Linked {
     }
     return current;
   }
+
+  size() {
+    let current = this.head;
+    if (current === null) return 0;
+    let listLength = 1;
+    while (current.nextNode !== null) {
+      current = current.nextNode;
+      listLength += 1;
+    }
+    return listLength;
+  }
 }
 
 class Node {
@@ -52,14 +63,19 @@ class Node {
 }
 console.clear();
 const first = new Linked();
+console.log("size", first.size());
+
 first.append("1");
 first.append("2");
 first.append("3");
+console.log("size", first.size());
+
 first.prepend("000");
-//console.log(first.append("4"));
+first.append("4");
 
 console.log("head", first.head1());
 console.log("tail", first.tail());
+console.log("size", first.size());
 
 // first.append("334");
 //first.append("22");
