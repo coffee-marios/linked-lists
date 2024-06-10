9a8bb880c0ac2c15490fa2eb34916130e992c871
coffee-marios <mark_theo@yahoo.com>
2024-06-10 13:06:52 +0300

    Create a function (contains(value) that returns true if the value is in the list and a function (find(value)) that returns its index if it is there

--- index.js
+++ index.js
@@ -53,6 +53,31 @@ class Linked {
     }
     return listLength;
   }
+
+  contains(value) {
+    let current = this.head;
+    if (current.value === value) return true;
+    while (current.nextNode) {
+      if (current.nextNode.value === value) return true;
+      current = current.nextNode;
+    }
+
+    return false;
+  }
+
+  find(value) {
+    let current = this.head;
+    let index = 0;
+    if (current === null) return null;
+    if (current.value === value) return 0;
+
+    while (current.nextNode !== null) {
+      index += 1;
+      if (current.nextNode.value === value) return index;
+      current = current.nextNode;
+    }
+    return null;
+  }
 }
 
 class Node {
@@ -76,6 +101,18 @@ first.append("4");
 console.log("head", first.head1());
 console.log("tail", first.tail());
 console.log("size", first.size());
+
+console.log('The index of "000" is', first.find("000"));
+console.log('The index of "1" is', first.find("1"));
+console.log('The index of "3" is', first.find("3"));
+console.log('The index of "4" is', first.find("4"));
+console.log('The index of "ab" is', first.find("ab"));
+
+console.log('The value "000" exists in the list:', first.contains("000"));
+console.log('The value "3" exists in the list:', first.contains("3"));
+
+console.log('The value "4" exists in the list:', first.contains("4"));
+console.log('The value "no" exists in the list:', first.contains("no"));
 
 // first.append("334");
 //first.append("22");
